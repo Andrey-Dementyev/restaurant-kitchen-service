b5a48e28f15a850783bfe5fbf9383f9d8af9daeb
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-11-30 10:14:19 +0200

    add a custom user model Cook that inherits from AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "dish type"
+        verbose_name_plural = "dish types"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}, (price: {self.price})"
