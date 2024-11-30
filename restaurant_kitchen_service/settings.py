5bc54c5ca4164a40ae467b796fff19d93117e567
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-11-30 09:23:49 +0200

    update installed apps and change time zone to Europe/Kiev

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "kitchen",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
