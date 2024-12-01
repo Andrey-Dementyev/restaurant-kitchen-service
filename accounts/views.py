5644e2fb661fec869f6d3189e3b8b5bd2e617700
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-12-01 18:39:03 +0200

    added search functional for cooks

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,29 @@
 from django.views import generic
 
+from accounts.forms import CookSearchForm
 from accounts.models import Cook
 
 
 class CookListView(generic.ListView):
     model = Cook
     paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all()
+        form = CookSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
 
 
 class CookDetailView(generic.DetailView):
