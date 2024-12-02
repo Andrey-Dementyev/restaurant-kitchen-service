e6842c7db4f2e1d07e6dac3167876d7b18df4a35
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-12-02 16:37:33 +0200

    added registration functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
+    path("activate/<str:uid>/<str:token>/", views.activate, name="activate"),
+
     path("cooks/", views.CookListView.as_view(), name="cook-list"),
     path("<int:pk>/", views.CookDetailView.as_view(), name="cook-detail"),
 ]
